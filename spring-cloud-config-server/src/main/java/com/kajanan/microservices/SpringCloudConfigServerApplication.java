f83dd2e79d446acc5046c0822891c4f851906516
kajananrajendran <kajananrajendran@localhost>
2024-07-13 20:53:23 +0530

    Added the updates

--- spring-cloud-config-server/src/main/java/com/kajanan/microservices/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/kajanan/microservices/SpringCloudConfigServerApplication.java
@@ -2,10 +2,12 @@ package com.kajanan.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableConfigServer
 @SpringBootApplication
+@ComponentScan(basePackages = "com.kajanan")
 public class SpringCloudConfigServerApplication {
 
 	public static void main(String[] args) {
