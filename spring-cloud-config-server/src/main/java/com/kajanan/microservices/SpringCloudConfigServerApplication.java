a4a7b10ab6b7f08de6860dfb2e2dc9a314cc12fd
kajananrajendran <kajananrajendran@localhost>
2024-07-13 13:55:35 +0530

    Added cloud config server

---
+++ spring-cloud-config-server/src/main/java/com/kajanan/microservices/SpringCloudConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.kajanan.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+
+}
