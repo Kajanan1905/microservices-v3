6a40b63b9d5d631cd914fc377b0b0fc52917ae74
kajananrajendran <kajananrajendran@localhost>
2024-07-14 13:13:29 +0530

    Added api-gateway

---
+++ api-gateway/src/test/java/com/kajanan/microservices/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kajanan.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
